9229f287220fdfb81efa73f1a52a134f166d0a0f
Brian <bmccormack15@gmail.com>
2021-11-26 05:16:39 -0800

    extended functionality to multiple currencies, changed url structure

--- forexValues.py
+++ forexValues.py
@@ -3,35 +3,38 @@ import urllib.request
 import json
 import matplotlib.pyplot as plt
 
-#oldurl = "https://www.bankofcanada.ca/valet/observations/group/FXUSDCAD/json?recent=5"
-
 def get_data(url):
 	with urllib.request.urlopen(url) as resp:
 		return json.loads(resp.read())
 
-def parse_data(rawData, option):
+def parse_data(rawData, option, exName):
 	obs = []
 	rawObs = rawData[option]
 	for item in rawObs:
-		obs.append((item['d'], item['FXCADUSD']['v']))
+		obs.append((item['d'], item[exName]['v']))
 	return obs
 	
-def plot_data(xaxis, yaxis):
+def plot_data(xaxis, yaxis, exName):
+	plt.xlabel('Date')
+	plt.ylabel('Value')
+	plt.title(exName[2:] + ' Foreign Exchange Value')
 	plt.plot(xaxis, yaxis)
 	plt.show()
 	plt.close()
 
 # might use less resources if all items are not stored in future (eg once done with CADUSD overwrite)
-urlItems = {"cadUsd": "https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?recent=5"}
-
-#parse_data assumes observations is in the format of a list of tuples each item is (dayi, valuei)
+#		idea give user the option of combining different plots in same chart (use legend)
+currencyList = ['FXCADUSD', 'FXCADEUR', 'FXCADGBP']
+numObs = 5 #number of observations
 observations = {}
-observations['cadUsd'] = parse_data(get_data(urlItems['cadUsd']), 'observations')
 
-#reverse the observations pair 
-dayList = [x[0] for x in reversed(observations['cadUsd'])]
-currList = [x[1] for x in reversed(observations['cadUsd'])]
-
-#overwrite currency value list with each item being coverted from str to float
-currList = list(map(lambda x: float(x), currList))
-plot_data(dayList, currList)
+for item in currencyList:
+	tmpUrl = "https://www.bankofcanada.ca/valet/observations/" + item + "/json?recent=" + str(numObs)
+	#parse_data assumes observations is in the format of a list of tuples each item is (dayi, valuei)
+	observations[item] = parse_data(get_data(tmpUrl), 'observations', item)
+	#reverse the observations pair 
+	dayList = [x[0] for x in reversed(observations[item])]
+	currList = [x[1] for x in reversed(observations[item])]
+	#overwrite currency value list with each item being coverted from str to float
+	currList = list(map(lambda x: float(x), currList))
+	plot_data(dayList, currList, item)
