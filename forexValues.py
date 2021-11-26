fb6d198c90ee0991426c449ec26db71c108aad97
Brian <bmccormack15@gmail.com>
2021-11-25 20:17:53 -0800

    added graphing of values over time

--- forexValues.py
+++ forexValues.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 import urllib.request
 import json
+import matplotlib.pyplot as plt
 
 #oldurl = "https://www.bankofcanada.ca/valet/observations/group/FXUSDCAD/json?recent=5"
 
-url = "https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?recent=5"
+def get_data(url):
+	with urllib.request.urlopen(url) as resp:
+		return json.loads(resp.read())
 
-#req = urllib.request.Request(url)
-
-with urllib.request.urlopen(url) as resp:
-	tmp = resp.read()
-	#print(tmp)
+def parse_data(rawData, option):
+	obs = []
+	rawObs = rawData['observations']
+	for item in rawObs:
+		obs.append((item['d'], item['FXCADUSD']['v']))
+	return obs
+		
+cadUsdUrl = "https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?recent=5"
+cadUsdObs = parse_data(get_data(cadUsdUrl), 'observations')
 	
-a = json.loads(tmp)
-obs = a['observations']
-for item in obs:
-	print(item['d'], ": ", item['FXCADUSD']['v'], sep="")
+#reverse the observations pair 
+dayList = [x[0] for x in reversed(cadUsdObs)]
+currList = [x[1] for x in reversed(cadUsdObs)]
+#overwrite currency value list with each item being coverted from str to float
+currList = list(map(lambda x: float(x), currList))
 
+plt.plot(dayList, currList)
+plt.show()
+plt.close()
